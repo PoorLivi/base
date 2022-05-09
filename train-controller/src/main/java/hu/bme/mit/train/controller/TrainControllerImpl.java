8c4f4b148ff586730be4ae370a0809ae0886ff06
Screeper11 <bencep4pp@gmail.com>
2022-05-09 17:10:31 +0200

    SonarQube setup

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,6 +2,9 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
@@ -23,8 +26,7 @@ public class TrainControllerImpl implements TrainController {
 		enforceSpeedLimit();
 	}
 
-	public void controlTrain()
-    {
+	public void controlTrain() {
                 Timer timer = new Timer();
                 timer.scheduleAtFixedRate(new TimerTask() {
                         @Override
